7fc9ecc269f1f03e189475ecf84523e54c85516a
yakumar <yakumar_in@yahoo.com>
2017-01-15 11:33:53 +0000

    first

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,42 @@
 var webpack = require('webpack');
 var path = require('path');
+const VENDOR_LIBS = [
+  'react', 'faker', 'lodash', 'react-redux', 'redux', 'react-dom', 'redux-form', 'redux-thunk', 'react-input-range'
+    
+]
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
-  entry: './src/index.js',
+  entry: {
+    bundle:'./src/index.js',
+    vendor:VENDOR_LIBS
+  },
   output: {
     path: path.join(__dirname, 'dist'),
-    filename: 'bundle.js'
-  }
+    filename: '[name].[chunkhash].js'
+  },
+  module:{
+    rules:[
+      {
+        use:'babel-loader',
+        test:/\.js$/,
+        exclude:/node_modules/
+      },
+      {
+        use:['style-loader', 'css-loader'],
+        test:/\.css$/
+      }
+    ]
+  },
+  plugins:[
+    new webpack.optimize.CommonsChunkPlugin({
+      names: ['vendor', 'manifest']
+    }),
+    new HtmlWebpackPlugin({
+      template: 'src/index.html'
+    }),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+    })
+  ]
 };
